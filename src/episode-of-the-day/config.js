7e1ef678c76697ed63b23a2040d7bb4b43be7282
Jonas Gierer <jgierer12@outlook.com>
2017-04-17 23:42:40 +0200

    Disable scheduled posts
    Since we're about to change the schedule for automatic episode announcements,
    we're disabling them for now.

--- src/episode-of-the-day/config.js
+++ src/episode-of-the-day/config.js
@@ -2,10 +2,6 @@ import moment from 'moment';
 
 export default {
 	startDate: moment('2017-04-14 12:00'),
-	schedule: {
-		hour: 12,
-		minute: 0,
-		second: 0
-	},
+	schedule: {year: -1},
 	defaultChannel: '#general'
 };
